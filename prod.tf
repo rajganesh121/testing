8c3e030797bfcb801a086189cdc05239c2c0d513
VANDRANGI <RAJASEKHAR.VANDRANGI@finastra.com>
2022-03-29 12:13:48 +0530

    added new files

--- prod.tf
+++ prod.tf
@@ -27,45 +27,45 @@ resource "aws_s3_bucket" "prod_tf_course" {
   bucket = "tf-course-202203200833"
 }
 
-resource "aws_default_vpc" "default" {}
+# resource "aws_default_vpc" "default" {}
 
-resource "aws_security_group" "prod_web"{
-  name        = "prod_web"
-  description = "Allow standrad http and https inbound and everything outbound"
+# resource "aws_security_group" "prod_web"{
+#   name        = "prod_web"
+#   description = "Allow standrad http and https inbound and everything outbound"
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = var.whitelist
-  }
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = var.whitelist
-  }
-  egress {
-    from_port    = 0
-    to_port      = 0
-    protocol     = "-1"
-    cidr_blocks  = var.whitelist
-  }
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = var.whitelist
+#   }
+#   ingress {
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = var.whitelist
+#   }
+#   egress {
+#     from_port    = 0
+#     to_port      = 0
+#     protocol     = "-1"
+#     cidr_blocks  = var.whitelist
+#   }
 
-  tags = {
-    "Terraform" : "true"
-  }
-}
+#   tags = {
+#     "Terraform" : "true"
+#   }
+# }
 
-resource "aws_instance" "prod_web" {
-  ami           = var.web_image_id
-  instance_type = var.web_instance_type
+# resource "aws_instance" "prod_web" {
+#   ami           = var.web_image_id
+#   instance_type = var.web_instance_type
 
-  vpc_security_group_ids = [
-    aws_security_group.prod_web.id
-   ]
+#   vpc_security_group_ids = [
+#     aws_security_group.prod_web.id
+#    ]
 
-  tags = {
-    "Terraform" : "true"
-  }
-}
+#   tags = {
+#     "Terraform" : "true"
+#   }
+# }
--- terraform.tfvars
+++
@@ -1,3 +0,0 @@
-whitelist 	= ["0.0.0.0/0"]
-web_image_id	= "ami-0069731c25ae3be5b"
-web_instance_type = "t2.nano"
