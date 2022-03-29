fe1d00ff95d1ca764fce00942b1fbe9e92ad67d4
VANDRANGI <RAJASEKHAR.VANDRANGI@finastra.com>
2022-03-29 14:38:34 +0530

    added new files

--- prod.tf
+++ prod.tf
@@ -35,6 +35,11 @@ output "s3_name" {
   value = aws_s3_bucket.prod_tf_course.tags
 }
 
+output "s3_name_value" {
+  value = s3_name.Name  
+}
+
+
 # resource "aws_s3_bucket" "prod_tf_course1" {
 #   bucket = "tf-course-202203200834"
 # }
